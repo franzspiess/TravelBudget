b9a98886daf8f9be48650cb95c8179c073f113c4
franzspiess <picnhepanchopincho@gmail.com>
2020-03-01 10:24:30 +0100

    Create mock trip

--- App.tsx
+++ App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { View, Button } from 'react-native';
 import styles from './src/styles'
 
+
 import TravelItemContainer from './src/interface/Components/TravelItemContainer'
+import { myTrip } from './src/infrastructure/Models/mockData';
 
 export default function App() {
+  const currentTrip = myTrip
   return (
     <View style={styles.container}>
-      <TravelItemContainer />
+      <TravelItemContainer trips={[myTrip]} />
       <TravelItemContainer />
       <TravelItemContainer />
       <Button
--- src/infrastructure/Models/Models.ts
+++ src/infrastructure/Models/Models.ts
@@ -1,26 +1,41 @@
-import faker from 'faker'
+import { v4 as uuidv4 } from 'uuid';
 
-class Trip {
+export class Trip {
   public days : Day[]
   public totalBudget: number
+  public startDate: Date
+  public endDate: Date
+  public duration: number
 
-  constructor(props) {
-    this.days = props.days
-    this.totalBudget = props.totalBudget
+  constructor(days, totalBudget, duration) {
+    this.days = days
+    this.totalBudget = totalBudget
+    this.startDate = days[0].date
+    this.endDate = days[-1].date
+    this.duration = duration
   }
 }
 
-class Day {
+export class Day {
   public id: string
   public dailyBudget : number
+  public date: Date
   public expenses : Expense[]
+
+  constructor(dailyBudget, date) {
+    this.id = uuidv4()
+    this.dailyBudget = dailyBudget
+    this.date = date
+  }
 
 }
 
-class Expense {
-  public id: string
+export class Expense {
   public type: ExpenseTypes
-  public cost: number
+  public expenses: {
+    cost: number
+    description: string
+  }
 }
 
 type ExpenseTypes = 'food' | 'nightlife' | 'tourism' | 'transport'
